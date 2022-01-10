25c460d74612d684a575967b5606064ec787d9e0
郭勇 <>
2022-01-10 15:13:48 +0800

    校验打开activity的包名

---
+++ app/src/main/java/com/gy/commonviewdemo/ext/ActivityExt.kt
@@ -0,0 +1,16 @@
+package com.gy.commonviewdemo.ext
+
+import android.app.Activity
+
+
+// 校验打开这个Activity 的是哪个应用打开的
+// 主要是处理通过Intent-Filter 打开Activity 的情况
+fun Activity.checkOpenPackage(){
+
+   val componentName =  callingActivity
+    if(componentName == null){
+        finish()
+    }else if(packageName !== componentName.packageName){
+        finish()
+    }
+}
