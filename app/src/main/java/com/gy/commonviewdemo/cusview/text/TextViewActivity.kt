da05fd6d5f0f21365d674bba6f5b742e6fa30b8a
郭勇 <>
2022-02-23 19:11:33 +0800

    会折叠的textView

--- app/src/main/java/com/gy/commonviewdemo/cusview/text/TextViewActivity.kt
+++ app/src/main/java/com/gy/commonviewdemo/cusview/text/TextViewActivity.kt
@@ -2,9 +2,12 @@ package com.gy.commonviewdemo.cusview.text
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.gy.commonviewdemo.R
 import kotlinx.android.synthetic.main.activity_textview.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class TextViewActivity : AppCompatActivity(){
@@ -16,6 +19,14 @@ class TextViewActivity : AppCompatActivity(){
         resolveHighLight()
         // 计算textView 的行数
         resolveLineCount()
+
+        animNumTextView()
+
+        autoFitTextView()
+
+        tv_expandable.postDelayed({
+            tv_expandable.text = "这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView这是可以收缩的textView"
+        },2000)
     }
 
     private fun resolveHighLight(){
@@ -29,6 +40,19 @@ class TextViewActivity : AppCompatActivity(){
         tv_line_count.text = content
         val count = tv_line_count.getLineCountByFontSize(content)
         Log.i("ccccccccccc","count===$count")
+    }
+
+    private fun animNumTextView(){
+        anim_tv.setNumberString("1000","10000")
+    }
+
+    private fun autoFitTextView(){
+        var content = "自适应宽度的 textView自适应宽度的 textView自适应宽度的 textView自适应宽度的 textView"
+        auto_fit_tv.text = content
+        auto_fit_tv.postDelayed({
+            content+="自适应宽度的 textView自适应宽度的 textView自适应宽度的 textView自适应宽度的 textView"
+            auto_fit_tv.text = content
+        },1000)
     }
 
 }
---
+++ app/src/main/java/com/gy/commonviewdemo/cusview/text/readMe.kt
@@ -0,0 +1,20 @@
+package com.gy.commonviewdemo.cusview.text
+
+
+// UI_RandomTextView
+// https://github.com/newPersonKing/UI_RandomTextView.git
+
+// UI_Animaetion__Text
+// https://github.com/newPersonKing/UI_Animaetion__Text.git
+
+// UI_MoneyEditText
+// https://github.com/newPersonKing/UI_MoneyEditText.git
+
+//左右对齐的textview AlignTextView
+//https://github.com/newPersonKing/AlignTextView.git
+
+//Android TextView with rolling animation
+//https://github.com/newPersonKing/RollingText.git
+
+//垂直的textview
+//https://github.com/newPersonKing/PlumbTextView.git
